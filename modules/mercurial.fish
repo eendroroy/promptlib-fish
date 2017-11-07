cbda8d33e63e11e5f833416a866b691bacac0874
indrajit <eendroroy@gmail.com>
2017-11-07 21:27:18 +0600

    mercurial

--- modules/mercurial.fish
+++ modules/mercurial.fish
@@ -5,11 +5,11 @@ function plib_is_hg
 end
 
 function plib_hg_branch
-  set -l __ref (hg branch 2> /dev/null) or return;
+  set -l __ref (hg branch 2> /dev/null); or return;
   echo -ne $__ref;
 end
 
 function plib_hg_rev
-  set -l __rev (hg identify --num 2>/dev/null | tr -d " +") or return;
+  set -l __rev (hg identify --num 2>/dev/null | tr -d " +"); or return;
   echo -ne $__rev;
 end
